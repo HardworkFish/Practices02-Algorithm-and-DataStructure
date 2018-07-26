20983b0a5156aba04e5a2ef682745cec7efbb2b0
Trsky <625310581@qq.com>
2018-07-26 10:25:47 +0800

    complete the recursive bubble sort

--- Python/sort/bubble/tests/bubble_sort_test.py
+++ Python/sort/bubble/tests/bubble_sort_test.py
@@ -4,15 +4,11 @@ sys.path.append('../sources/')
 from sort.bubble.sources.bubbleSort import basic_bubble
 from sort.bubble.sources.bubbleSort import recursive_bubble
 arr = [64,34,25,12,22,11,90]
-start = time.time()
 arr = basic_bubble(arr)
-end = time.time()
 print("Basic Sorted array is: %s" % str(arr))
 
 ##### Python 递归冒泡排序 #######
 arr = [64,34,25,12,22,11,90]
-start = time.time()
 arr = recursive_bubble(arr)
-end = time.time()
 print("Recursive Sorted array is: %s" % str(arr))
 
