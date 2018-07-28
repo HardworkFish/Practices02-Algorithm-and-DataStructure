ccd5cfde8d358c0da7500b79119eb5bd1a2c4992
Trsky <625310581@qq.com>
2018-07-28 23:17:20 +0800

    add Description about shell sorts

--- Python/sort/sources/shellSort.py
+++ Python/sort/sources/shellSort.py
@@ -3,15 +3,20 @@
 
 def shell_sort(arr):
     n = len(arr)
-    gap = int(n/2)
+    gap = int(n/2) # 初始化 gap 值
+    # 第一层循环：依次改变 gap 值对列表进行分组
     while gap>0:
-        for i in range(gap,n):
+    # 下面：利用直接插入排序的思想对分组元素进行排序
+    # range(gap, n) 从 gap 开始
+        for i in range(gap, n):
             temp = arr[i]
             j = i
+            # 类似插入排序, 当前值与指定步长之前的值比较, 符合条件则交换位置
             while j>=gap and arr[j-gap]>temp:
                 arr[j] = arr[j-gap]
                 j -= gap
             arr[j] = temp
+        # while 循环条件折半
         gap=int(gap/2)
     return arr
 
